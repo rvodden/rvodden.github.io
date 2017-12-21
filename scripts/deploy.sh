bee8c30c19fbe7472580973bb5cd2b17de911eae
Richard Vodden <richard@vodden.com>
2017-12-21 13:37:14 +0000

    More build fiddling

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,17 +4,13 @@ set -eux pipefail
 # Variables
 ORIGIN_URL=$(git config --get remote.origin.url 2>&1)
 
-if [ -n ${?}  ]; then
+if [ 0 -ne ${?}  ]; then
     echo "Failed to get origin URL"
     echo $ORIGIN_URL
     exit -1
 fi
 
-# Trust github
-mkdir -p ~/.ssh
-echo <<EOF >> ~/.ssh/known_hosts
-github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==
-EOF
+GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no"
 
 echo "Started deploying"
 
@@ -38,7 +34,7 @@ echo "vodden.com" > CNAME
 
 git add -fA
 git commit --allow-empty -m "$(git log -1 --pretty=%B) [ci skip]"
-yes | git push -f $ORIGIN_URL master
+git push --no-verify -f $ORIGIN_URL master
 
 # Move back to previous branch.
 git checkout -
