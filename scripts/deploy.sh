e0150e4dbe559df60eb4aac3cbfd7eb25efb46fd
Richard Vodden <richard@vodden.com>
2017-12-21 12:19:19 +0000

    Persisted .circleci in master branch

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,7 +14,7 @@ fi
 git checkout -b master
 
 # Delete and move files.
-find . -maxdepth 1 ! -name '_site' ! -name '.git' ! -name '.gitignore' -exec rm -rf {} \;
+find . -maxdepth 1 ! -name .circleci ! -name '_site' ! -name '.git' ! -name '.gitignore' -exec rm -rf {} \;
 mv _site/* .
 rm -R _site/
 
